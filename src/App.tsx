a05a900b8c4868dda2a48992b5d610135f311abd
Kekyra228 <katyiv02@gmail.com>
2024-10-29 15:17:13 +0400

    первоначальный визуал

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="calc">
+        <form action="">
+          <div className="display">
+            <input type="text" />
+          </div>
+          <div>
+            <input type="button" value="delete" />
+          </div>
+          <div>
+            <input type="button" value="7" />
+            <input type="button" value="8" />
+            <input type="button" value="9" />
+            <input type="button" value="*" />
+          </div>
+          <div>
+            <input type="button" value="4" />
+            <input type="button" value="5" />
+            <input type="button" value="6" />
+            <input type="button" value="+" />
+          </div>
+          <div>
+            <input type="button" value="1" />
+            <input type="button" value="2" />
+            <input type="button" value="3" />
+            <input type="button" value="-" />
+          </div>
+          <div>
+            <input type="button" value="0" />
+            <input type="button" value="00" />
+            <input type="button" value="=" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
